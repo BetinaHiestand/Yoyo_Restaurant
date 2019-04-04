5ac98c4bc544575b4835c8983e20c9f7507733f7
BetinaHiestand <45491928+BetinaHiestand@users.noreply.github.com>
2019-04-04 16:10:03 +0200

    Add files via upload

--- Client.java
+++ Client.java
@@ -3,53 +3,46 @@ package com.yoyo;
 import java.util.Calendar;
 import java.util.concurrent.TimeUnit;
 
-public class Client implements Talking{
-	
+public class Client implements Talking {
+
 	private String name;
-	
+	private String daytime;
+
+	// Checking for current time
+	Calendar now = Calendar.getInstance();
+	int hour = now.get(Calendar.HOUR_OF_DAY);
 
 	public String getName() {
 		return name;
 	}
 
-
 	public Client(String name) {
 		this.name = name;
 	}
 
-
 	@Override
 	public void welcome() throws InterruptedException {
-		
-		String daytime;
-		
-		//Checking for current time and deciding which daytime it is 
-		Calendar now = Calendar.getInstance();
-		int hour = now.get(Calendar.HOUR_OF_DAY);
-		
-		if (hour > 6 && hour < 12)
-		{
+
+		// Deciding which daytime it is
+
+		if (hour > 6 && hour < 12) {
 			daytime = "morning";
-		}
-		else if (hour > 12 && hour < 17) {
+		} else if (hour > 12 && hour < 17) {
 			daytime = "afternoon";
-		}
-		else {
+		} else {
 			daytime = "evening";
 		}
-	
-		
-		//Say Hello to the Waiter
+
+		// Say Hello to the Waiter
 		System.out.println("Client: Good " + daytime);
 		TimeUnit.SECONDS.sleep(2);
-		
-	}
 
+	}
 
 	@Override
 	public void goodbye() {
 		System.out.println("Client: Thank you for the delicous food and goodbye");
-		
+
 	}
 
 }
